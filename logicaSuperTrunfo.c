720284dfb3abe587504471254f91fab5c8f4f952
gabrielstudentdev <jgsilvatelles280802@gmail.com>
2025-04-05 21:39:43 +0000

    Objetivo O programa simula uma partida simples do jogo "Super Trunfo", onde dois países fictícios são comparados com base em atributos gerados aleatoriamente. O jogador escolhe dois atributos diferentes para comparar, e o programa determina o país vencedor com base nesses critérios.
    Geração de Dados
    O programa gera valores aleatórios entre 1 e 100 para três atributos de dois países:
    
    População
    
    PIB (Produto Interno Bruto)
    
    Densidade Demográfica
    
    Esses valores são gerados por meio da função rand(), inicializada com o tempo atual para garantir aleatoriedade a cada execução.
    
    Etapas do Programa
    Mensagem de Boas-Vindas
    O programa inicia exibindo uma mensagem introdutória ao jogador.
    
    Escolha do Primeiro Atributo
    O usuário escolhe um dos três atributos para comparar:
    
    'P' ou 'p': População
    
    'B' ou 'b': PIB
    
    'D' ou 'd': Densidade Demográfica
    
    Com base na escolha:
    
    Para População e PIB, vence o país com o maior valor.
    
    Para Densidade Demográfica, vence o país com o menor valor.
    
    O país vencedor recebe 1 ponto.
    
    Escolha do Segundo Atributo
    O jogador escolhe um segundo atributo diferente do primeiro.
    Se o jogador repetir o mesmo atributo, o programa exibe uma mensagem de erro e encerra.
    Caso contrário, realiza nova comparação seguindo as mesmas regras do primeiro atributo e atribui mais um ponto ao país vencedor, se houver.
    
    Exibição dos Resultados
    Após as comparações, o programa exibe:
    
    Os valores de População, PIB e Densidade Demográfica dos dois países.
    
    O placar final com a pontuação de cada país.
    
    O resultado da partida, que pode ser:
    
    Vitória do País 1
    
    Vitória do País 2
    
    Empate
    
    Tratamento de Erros
    Caso o jogador escolha um caractere inválido para o atributo (diferente de P, B ou D), o programa exibe uma mensagem de erro e encerra.
    
    Caso o jogador escolha o mesmo atributo duas vezes, o programa avisa e termina a execução sem fazer a segunda comparação.
    
    Considerações Finais
    O programa utiliza estruturas simples de controle, como switch e if-else, e interage com o usuário via entrada padrão (scanf) e saída (printf). A aleatoriedade dos dados garante que cada execução do jogo seja diferente da anterior. O código é ideal como exercício introdutório à linguagem C, lógica de comparação e manipulação de entrada de usuário.
    
    Se quiser, posso ajudar a transformar essa documentação em PDF, adicionar fluxogramas ou sugerir melhorias para o código.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,123 +1,104 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    // Variáveis para as cartas
-    char estado1[40], cidade1[40], estado2[40], cidade2[40];
-    int populacao1, populacao2, pontosturisticos1, pontosturisticos2;
-    float area1, area2, pib1, pib2, densidadepopulacional1, densidadepopulacional2;
-    int escolha;
+    int pontos1 = 0, pontos2 = 0;
+    char primeiroAtributo, segundoAtributo;
+    int populacao1, populacao2, pib1, pib2, densidade1, densidade2;
 
-    // Entrada de dados para a primeira carta
-    printf("Digite o nome do estado da primeira carta: ");
-    scanf("%39s", estado1);
-    printf("Digite o nome da cidade da primeira carta: ");
-    scanf("%39s", cidade1);
-    printf("Digite a população da primeira carta: ");
-    scanf("%d", &populacao1);
-    
-    printf("Digite a área (km²) da primeira carta: ");
-    scanf("%f", &area1);
-    
-    printf("Digite o PIB da primeira carta: ");
-    scanf("%f", &pib1);
-    
-    printf("Digite o número de pontos turísticos da primeira carta: ");
-    scanf("%d", &pontosturisticos1);
+    srand(time(0)); // Gera números aleatórios diferentes a cada execução
 
-    // Entrada de dados para a segunda carta
-    printf("\nDigite o nome do estado da segunda carta: ");
-    scanf("%39s", estado2);
-    printf("Digite o nome da cidade da segunda carta: ");
-    scanf("%39s", cidade2);
-    printf("Digite a população da segunda carta: ");
-    scanf("%d", &populacao2);
-    
-    printf("Digite a área (km²) da segunda carta: ");
-    scanf("%f", &area2);
-    
-    printf("Digite o PIB da segunda carta: ");
-    scanf("%f", &pib2);
-    
-    printf("Digite o número de pontos turísticos da segunda carta: ");
-    scanf("%d", &pontosturisticos2);
+    // Geração de valores aleatórios entre 1 e 100
+    populacao1 = rand() % 100 + 1;
+    populacao2 = rand() % 100 + 1;
+    pib1 = rand() % 100 + 1;
+    pib2 = rand() % 100 + 1;
+    densidade1 = rand() % 100 + 1;
+    densidade2 = rand() % 100 + 1;
 
-    // Cálculo da densidade populacional
-    densidadepopulacional1 = populacao1 / area1;
-    densidadepopulacional2 = populacao2 / area2;
+    printf("Bem-vindo ao Super Trunfo de Países!\n\n");
 
-    // Menu interativo
-    printf("\nEscolha um atributo para comparar:\n");
-    printf("1 - População\n");
-    printf("2 - Área\n");
-    printf("3 - PIB\n");
-    printf("4 - Pontos turísticos\n");
-    printf("5 - Densidade populacional\n");
-    printf("Opção: ");
+    // PRIMEIRO ATRIBUTO
+    printf("Escolha o primeiro atributo para comparar:\n");
+    printf("P. População\nB. PIB\nD. Densidade Demográfica\n");
+    printf("Escolha a comparação: ");
+    scanf(" %c", &primeiroAtributo);
 
-    // Adicionando validação para evitar entrada inválida
-    if (scanf("%d", &escolha) != 1) {
-        printf("Entrada inválida! Certifique-se de digitar um número.\n");
-        return 1;  // Sai do programa caso a entrada seja inválida
+    switch (primeiroAtributo) {
+    case 'P':
+    case 'p':
+    printf("\nVocê escolheu População!\n");
+    if (populacao1 > populacao2) pontos1++;
+    else if (populacao2 > populacao1) pontos2++;
+    break;
+    case 'B':
+    case 'b':
+    printf("\nVocê escolheu PIB!\n");
+    if (pib1 > pib2) pontos1++;
+    else if (pib2 > pib1) pontos2++;
+    break;
+    case 'D':
+    case 'd':
+    printf("\nVocê escolheu Densidade Demográfica!\n");
+    if (densidade1 < densidade2) pontos1++;
+    else if (densidade2 < densidade1) pontos2++;
+    break;
+    default:
+    printf("\nOpção inválida!\n");
+    return 1;
     }
 
-    switch (escolha) {
-        case 1:
-            printf("\nPopulação:\n");
-            printf("%s: %d habitantes\n", estado1, populacao1);
-            printf("%s: %d habitantes\n", estado2, populacao2);
-            if (populacao1 > populacao2)
-                printf("Vencedor: %s\n", estado1);
-            else if (populacao2 > populacao1)
-                printf("Vencedor: %s\n", estado2);
-            else
-                printf("Empate!\n");
-            break;
-        case 2:
-            printf("\nÁrea:\n");
-            printf("%s: %.2f km²\n", estado1, area1);
-            printf("%s: %.2f km²\n", estado2, area2);
-            if (area1 > area2)
-                printf("Vencedor: %s\n", estado1);
-            else if (area2 > area1)
-                printf("Vencedor: %s\n", estado2);
-            else
-                printf("Empate!\n");
-            break;
-        case 3:
-            printf("\nPIB:\n");
-            printf("%s: %.2f\n", estado1, pib1);
-            printf("%s: %.2f\n", estado2, pib2);
-            if (pib1 > pib2)
-                printf("Vencedor: %s\n", estado1);
-            else if (pib2 > pib1)
-                printf("Vencedor: %s\n", estado2);
-            else
-                printf("Empate!\n");
-            break;
-        case 4:
-            printf("\nPontos turísticos:\n");
-            printf("%s: %d pontos turísticos\n", estado1, pontosturisticos1);
-            printf("%s: %d pontos turísticos\n", estado2, pontosturisticos2);
-            if (pontosturisticos1 > pontosturisticos2)
-                printf("Vencedor: %s\n", estado1);
-            else if (pontosturisticos2 > pontosturisticos1)
-                printf("Vencedor: %s\n", estado2);
-            else
-                printf("Empate!\n");
-            break;
-        case 5:
-            printf("\nDensidade populacional:\n");
-            printf("%s: %.2f hab/km²\n", estado1, densidadepopulacional1);
-            printf("%s: %.2f hab/km²\n", estado2, densidadepopulacional2);
-            if (densidadepopulacional1 < densidadepopulacional2)
-                printf("Vencedor: %s\n", estado1);
-            else if (densidadepopulacional2 < densidadepopulacional1)
-                printf("Vencedor: %s\n", estado2);
-            else
-                printf("Empate!\n");
-            break;
-        default:
-            printf("Opção inválida! Tente novamente.\n");
+    // SEGUNDO ATRIBUTO
+    printf("\nEscolha o segundo atributo (diferente do primeiro):\n");
+    printf("P. População\nB. PIB\nD. Densidade Demográfica\n");
+    printf("Escolha a comparação: ");
+    scanf(" %c", &segundoAtributo);
+
+    if (primeiroAtributo == segundoAtributo) {
+    printf("\nVocê escolheu o mesmo atributo duas vezes. Encerrando.\n");
     }
+
+    switch (segundoAtributo) {
+    case 'P':
+    case 'p':
+    printf("\nVocê escolheu População!\n");
+    if (populacao1 > populacao2) pontos1++;
+    else if (populacao2 > populacao1) pontos2++;
+    break;
+    case 'B':
+    case 'b':
+    printf("\nVocê escolheu PIB!\n");
+    if (pib1 > pib2) pontos1++;
+    else if (pib2 > pib1) pontos2++;
+    break;
+    case 'D':
+    case 'd':
+    printf("\nVocê escolheu Densidade Demográfica!\n");
+    if (densidade1 < densidade2) pontos1++;
+    else if (densidade2 < densidade1) pontos2++;
+    break;
+    default:
+    printf("\nOpção inválida!\n");
+    }
+
+    // RESULTADOS
+    printf("\n=== RESULTADO DAS CARTAS ===\n");
+    printf("População: País 1 = %d | País 2 = %d\n", populacao1, populacao2);
+    printf("PIB:        País 1 = %d | País 2 = %d\n", pib1, pib2);
+    printf("Densidade:  País 1 = %d | País 2 = %d\n", densidade1, densidade2);
+
+    printf("\n=== PLACAR FINAL ===\n");
+    printf("País 1: %d ponto(s)\n", pontos1);
+    printf("País 2: %d ponto(s)\n", pontos2);
+
+    if (pontos1 > pontos2) {
+    printf("\nParabéns! Você venceu!\n");
+    } else if (pontos2 > pontos1) {
+    printf("\nVocê perdeu! País 2 venceu!\n");
+    } else {
+    printf("\nEmpate!\n");
+    }
+
     return 0;
 }
